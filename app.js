47d88f1097ccf4882465298b5d755387fd8097e4
ranupl <ranupl542011@gmail.com>
2023-12-13 13:29:53 +0530

    server created

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Get started with todo");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}/`);
+});
