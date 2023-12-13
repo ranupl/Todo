e56c0a5d5ca4f2e7bf5bce5e42c29ebc56e7ca2e
ranupl <ranupl542011@gmail.com>
2023-12-13 15:40:01 +0530

    user crud creation

---
+++ src/controller/userController.js
@@ -0,0 +1,145 @@
+const httpError = require("http-errors");
+const { UserDB } = require("../model/userModel");
+
+async function createUser(req, res, next) {
+  const { firstname, lastname, email, username, password } = req.body;
+  try {
+    const user = await UserDB.findOne({ email });
+
+    if (user) {
+      return next(httpError.BadRequest("User already exits.."));
+    }
+
+    const newUser = new UserDB({
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      username: username,
+      password: password,
+    });
+
+    await newUser.save();
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: req.body,
+    });
+  } catch (err) {
+    return next(httpError.InternalServerError(err.message));
+  }
+}
+
+async function getAllUser(req, res, next) {
+  const user = await UserDB.find();
+  try {
+    if (user.length > 0) {
+      return res.status(200).json({
+        success: true,
+        message: "All users",
+        data: user,
+      });
+    }
+    return next(httpError.NotFound("No users"));
+  } catch (err) {
+    return next(httpError.InternalServerError(err.message));
+  }
+}
+
+async function getById(req, res, next) {
+  const { id } = req.params;
+  const user = await UserDB.findOne({ id });
+  try {
+    if (!user) {
+      return next(http.NotFound("User not found"));
+    }
+    return res.status(200).json({
+      success: true,
+      message: "User by Id",
+      data: user,
+    });
+  } catch (err) {
+    return next(httpError.InternalServerError(err.message));
+  }
+}
+
+async function updateUser(req, res, next) {
+  const { id } = req.params;
+
+  try {
+    let user = await UserDB.findOne({ id });
+    if (!user) {
+      return next(httpError.NotFound("No user found"));
+    }
+
+    user = await UserDB.findOneAndUpdate({ id }, req.body, { new: true });
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: req.body,
+    });
+  } catch (err) {
+    return next(httpError.InternalServerError(err.message));
+  }
+}
+
+async function deleteUser(req, res, next) {
+  const { id } = req.params;
+  const user = await UserDB.findOneAndDelete({ id });
+
+  try {
+    if (!user) {
+      return next(httpError.NotFound("User not found"));
+    }
+    return res.status(200).json({
+      success: true,
+      message: "user deleted",
+      data: req.user,
+    });
+  } catch (err) {
+    return next(httpError.InternalServerError(err.message));
+  }
+}
+
+async function filterResult(req, res, next) {
+  console.log(req.query);
+  try {
+    const { firstname, lastname, email, username } = req.query;
+    const filter = {};
+
+    if (firstname) {
+      filter.firstname = firstname;
+    }
+    if (lastname) {
+      filter.lastname = lastname;
+    }
+    if (email) {
+      filter.email = email;
+    }
+    if (username) {
+      filter.username = username;
+    }
+
+    const user = await UserDB.findOne(filter);
+
+    if (!user) {
+      return next(httpError.NotFound("User not found"));
+    }
+    return res.status(200).json({
+      success: true,
+      message: "user found",
+      data: user,
+    });
+  } catch (err) {
+    return next(httpError.InternalServerError(err.message));
+  }
+}
+
+module.exports = {
+  createUser,
+  getAllUser,
+  getById,
+  updateUser,
+  deleteUser,
+  filterResult,
+};
--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const route = express.Router();
 const todoController = require("../controller/todoController");
+const userController = require("../controller/userController");
 
-route.post("/create", todoController.createTodo);
+// user route
+route.post("/createUser", userController.createUser);
+route.get("/users", userController.getAllUser);
+route.get("/user/:id", userController.getById);
+route.put("/updateUser/:id", userController.updateUser);
+route.delete("/deleteUser/:id", userController.deleteUser);
+route.get("/filter", userController.filterResult);
+
+// todo route
+route.post("/createTodo", todoController.createTodo);
 route.get("/todos", todoController.getAllTodo);
 route.get("/todo/:id", todoController.getTodoById);
 route.put("/updateTodo/:id", todoController.updateTodo);
